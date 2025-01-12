c7d911bcee18e8769b776fff8a3478cf7614e099
DominikSukar <dominik.sukar@gmail.com>
2025-01-12 15:01:38 +0100

    refactor: change test database from sqlite to postgresql

--- backend/tests/setup.py
+++ backend/tests/setup.py
@@ -2,20 +2,23 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.pool import StaticPool
+from sqlalchemy_utils import database_exists, create_database, drop_database
 
 from models._database import Base, get_db
 from main import app
+from utils.env import DATABASE_LOGIN, DATABASE_PASSWORD
 
 
 @pytest.fixture(scope="class")
 def mock_client():
-    SQLALCHEMY_DATABASE_URL = "sqlite://"
-    engine = create_engine(
-        SQLALCHEMY_DATABASE_URL,
-        connect_args={"check_same_thread": False},
-        poolclass=StaticPool,
+    SQLALCHEMY_DATABASE_URL = (
+        f"postgresql://{DATABASE_LOGIN}:{DATABASE_PASSWORD}@localhost:5432/quickersim_test_database"
     )
+
+    if not database_exists(SQLALCHEMY_DATABASE_URL):
+        create_database(SQLALCHEMY_DATABASE_URL)
+    
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
     TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
     Base.metadata.create_all(bind=engine)
 
@@ -33,3 +36,4 @@ def mock_client():
     yield client
 
     Base.metadata.drop_all(bind=engine)
+    drop_database(SQLALCHEMY_DATABASE_URL)
