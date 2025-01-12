d38444c5879b7d5b5c193d306b554e909dd197b0
DominikSukar <dominik.sukar@gmail.com>
2025-01-12 16:54:13 +0100

    tests: adjust put test and add http422 checks for invalid mass and date reading posts

--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -4,40 +4,62 @@ from tests.setup import mock_client  # noqa: F401
 
 
 @pytest.fixture(scope="class")
-def sample_reading_data():
+def sample_reading():
     "Sample fixture returns an examplary mass data"
-    return {"date": "2025-01-03", "mass": 5.0}
+    return {"date": "2025-01-03", "mass": 55.0}
+
+@pytest.fixture(scope="class")
+def sample_reading_invalid_mass():
+    "Sample fixture returns mass data with invalid mass"
+    return {"date": "2025-01-03", "mass": -55.2}
+
+@pytest.fixture(scope="class")
+def sample_reading_invalid_date():
+    "Sample fixture returns mass data with invalid date"
+    return {"date": "2026-02-05", "mass": 55.0}
 
 
 class TestMassReading:
-    def test_post(self, mock_client, sample_reading_data):
-        response = mock_client.post("/readings/", json=sample_reading_data)
-        assert response.status_code == 201
+    def test_post_201(self, mock_client, sample_reading):
+        response = mock_client.post("/readings/", json=sample_reading)
 
         mass_reading = response.json()
-        print(mass_reading)
 
-        assert mass_reading["date"] == sample_reading_data["date"]
-        assert mass_reading["mass"] == sample_reading_data["mass"]
+        assert response.status_code == 201
+        assert mass_reading["date"] == sample_reading["date"]
+        assert mass_reading["mass"] == sample_reading["mass"]
 
-    def test_list(self, mock_client, sample_reading_data):
+    def test_post_invalid_mass(self, mock_client, sample_reading_invalid_mass):
+        response = mock_client.post("/readings/", json=sample_reading_invalid_mass)
+
+        assert response.status_code == 422
+
+    def test_post_invalid_date(self, mock_client, sample_reading_invalid_date):
+        response = mock_client.post("/readings/", json=sample_reading_invalid_date)
+
+        assert response.status_code == 422        
+
+    def test_list(self, mock_client, sample_reading):
         response = mock_client.get("/readings/")
-        assert response.status_code == 200
-
+        
         first_object = response.json()[0]
 
-        assert first_object["date"] == sample_reading_data["date"]
-        assert first_object["mass"] == sample_reading_data["mass"]
+        assert response.status_code == 200
+        assert first_object["date"] == sample_reading["date"]
+        assert first_object["mass"] == sample_reading["mass"]
 
-    def test_delete(self, mock_client, sample_reading_data):
-        delete_response = mock_client.delete(
-            f"/readings/{sample_reading_data['date']}/"
+    def test_delete(self, mock_client, sample_reading):
+        response = mock_client.delete(
+            f"/readings/{sample_reading['date']}/"
         )
-        assert delete_response.status_code == 200
+        assert response.status_code == 200
 
-    def test_put(self, mock_client, sample_reading_data):
+    def test_put(self, mock_client, sample_reading):
+        post_response = mock_client.post("/readings/", json=sample_reading) 
+        assert post_response.status_code == 201
+
         update_data = {"mass": 76.5}
         response = mock_client.put(
-            f"/readings/{sample_reading_data['date']}/", json=update_data
+            f"/readings/{sample_reading['date']}/", json=update_data
         )
         assert response.status_code == 200
