e965cabef8c5a1dd32b14b0e6dde070a7ce91c2e
DominikSukar <dominik.sukar@gmail.com>
2025-01-12 17:46:01 +0100

    feat: add cors middleware to backend

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers import (
     readings
 )
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"]
+)
 
 app.include_router(readings.router, tags=["Readings"], prefix="/readings")
